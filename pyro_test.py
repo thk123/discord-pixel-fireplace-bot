855554f5b8e128de51559aa1fb7ffd9b14090c9c
Thomas Kiley <thomas.kiley@diffblue.com>
2020-08-01 12:08:19 +0100

    Test capitalization

--- pyro_test.py
+++ pyro_test.py
@@ -7,7 +7,7 @@ class TestPyro(unittest.TestCase):
         self.assertEqual(pyro.extract_message('not starting with prefix', '!'), None)
         self.assertEqual(pyro.extract_message('!starting with prefix', '!'), 'starting with prefix')
         self.assertEqual(pyro.extract_message('no prefix', ''), 'no prefix')
-        self.assertEqual(pyro.extract_message('@pyro hello', '@pyro'), 'hello')
+        self.assertEqual(pyro.extract_message('@pyro hello', '@Pyro'), 'hello')
 
     def test_arrow_keys(self):
         self.assertTrue(pyro.is_arrow_keys('left'))
